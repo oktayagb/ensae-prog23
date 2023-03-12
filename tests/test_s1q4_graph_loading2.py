872eabfac60f82470c99d20842eb62c082236928
oktayagb <126254961+oktayagb@users.noreply.github.com>
2023-03-12 14:27:20 +0100

    Update test_s1q4_graph_loading2.py

--- tests/test_s1q4_graph_loading2.py
+++ tests/test_s1q4_graph_loading2.py
@@ -1,1 +1,17 @@
-# À compléter
+import sys
+sys.path.append("delivery_network/")
+
+import unittest
+from main import Graph, graph_from_file
+
+class Test_GraphLoading(unittest.TestCase):
+    def test_network4(self):
+        g = graph_from_file("input/network.04.in")
+        self.assertEqual(g.graph, {1: [(4, 11, 6), (2, 4, 89)], 2: [(3, 4, 3), (1, 4, 89)], 3: [(2, 4, 3), (4, 4, 2)], 4: [(3, 4, 2), (1, 11, 6)], 5: [], 6: [], 7: [], 8: [], 9: [], 10: []}
+)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
