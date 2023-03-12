265ac002eecfe642cef1cf24087633d7316fb26b
oktayagb <126254961+oktayagb@users.noreply.github.com>
2023-03-12 15:02:31 +0100

    Update test_s1q4_graph_loading2.py

--- tests/test_s1q4_graph_loading2.py
+++ tests/test_s1q4_graph_loading2.py
@@ -1,12 +1,12 @@
 import sys
-sys.path.append("delivery_network/")
+sys.path.append("ensae-prog23-main/")###peut etre qu'il faudra changer ici mais sur mon ordi ca marche
 
 import unittest
 from main import Graph, graph_from_file
 
 class Test_GraphLoading(unittest.TestCase):
     def test_network4(self):
-        g = graph_from_file("input/network.04.in")
+        g = graph_from_file("network.04.in")
         self.assertEqual(g.graph, {1: [(4, 11, 6), (2, 4, 89)], 2: [(3, 4, 3), (1, 4, 89)], 3: [(2, 4, 3), (4, 4, 2)], 4: [(3, 4, 2), (1, 11, 6)], 5: [], 6: [], 7: [], 8: [], 9: [], 10: []}
 )
 
@@ -14,4 +14,3 @@ class Test_GraphLoading(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-
