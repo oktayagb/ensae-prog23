ff0e98d5ce9d5a79a29db9a0a4c2d4814c8cb0b0
adrienmathieu <126259878+adrienmathieu@users.noreply.github.com>
2023-03-15 17:36:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ class Graph:
         self.nb_edges = 0
         self.edges = []
 
+        
     def __str__(self):
         """Prints the graph as a list of neighbors for each node (one per line)"""
         if not self.graph:
@@ -25,6 +26,7 @@ class Graph:
                 output += f"{source}-->{destination}\n"
         return output
 
+    
     def add_edge(self, node1, node2, power_min=1, dist=1):
         if node1 not in self.nodes:
             self.nodes.append(node1)
@@ -37,7 +39,8 @@ class Graph:
         self.nb_edges += 1
         self.graph[node1].append((node2, power_min, dist))
         self.graph[node2].append((node1, power_min, dist))
-        self.edges.append((node1, node2, power_min))
+        self.edges.append((node1, node2, power_min)) #on rajoute un élément 'edges' qui est une liste de tuple qui contient chaque chemin, avec leur 
+        # puissance associée
 
 
 # On va réaliser un algorithme de parcours en largeur, qui est plus intéressant à utiliser qu'un algorithme de parcours en profondeur car nous n'allons pas avoir
@@ -57,14 +60,16 @@ class Graph:
                         queue.extend(neighbour[0] for neighbour in self.graph[current] if neighbour[0] not in visited)
                 components.append(component)
         return components  # on renvoie les composantes connexes du graphe
-
     # Complexité en 0(nb_edges + nb_nodes)
 
+    
+    
     def connected_components_set(self):
         return set(map(frozenset,
                        self.connected_components()))  # on fait juste un frozenset de la fonction connected_components
 
 
+    
  # De la même manière que précédemment, nous allons avoir recourt à un algorithme de parcours en largeur afin de résoudre notre problème, de type Djikstra.
     def get_path_with_power(self, src, dest, power):
         queue = deque([(src, [src])])  # on met en place une pile afin de gérer les noeuds. L'intérêt de la pile est que l'on garde l'odre d'arrivée
@@ -78,9 +83,10 @@ class Graph:
                     visited.add(neighbor)  # on le marque comme visité
                     queue.append((neighbor, path + [neighbor]))  # on rajoute à la pile ce noeud et on actualise le parcours en rajoutant le noeud
         return None  # on renvoie None seulement si après avoir tout parcouru on ne trouve pas de chemin de puissance minimal, ou que les éléments sont dans 2 compo connexes
-
     # La complexité est en O(nb_edges)
 
+    
+    
     # Nous allons utiliser notre fonction get_path_with_power avec une recherche par dichotomie afin de trouver la puissance minimale.
     def min_power(self, src, dest):
         test = False
@@ -118,18 +124,18 @@ class Graph:
                         else:
                             mid = (len(puis) // 2) + 1
                         power_min = puis[mid - 1]
-                if len(
-                        puis) >= 2:  # on réalise la dichotomie en fonction de si la puissance donnée permet de réaliser le chemin.
+                if len(puis) >= 2:  # on réalise la dichotomie en fonction de si la puissance donnée permet de réaliser le chemin.
                     return (self.get_path_with_power(src, dest, puis[1]), puis[1])
                 elif len(puis) == 1:
                     return (self.get_path_with_power(src, dest, puis[0]), puis[0])
 # La complexité est de 0((nb_nodes+nb_egdes)*ln(nb_edges))
 
+
     def kruskal(self):
         edges = self.edges
         edges.sort(key=lambda x: x[2])  # on trie les chemins par ordre croissant de puissance
         parents = {node: node for node in self.nodes}  # on initialise les parents de chaque noeud.
-        rang = {node: 0 for node in self.nodes}
+        rang = {node: 0 for node in self.nodes} #on itinialise le rang de chaque noeud
         def find(node):
             if parents[node] != node:
                 parents[node] = find(parents[node])
@@ -143,7 +149,8 @@ class Graph:
                 else:
                     parents[y_racine] = x_racine
                     if rang[x_racine] == rang[y_racine]:
-                        rang[x_racine] = rang[x_racine] + 1
+                        rang[x_racine] = rang[x_racine] + 1 #la fonction union permet de lier les noeuds entre eux, l'utilisation de 'racine' 
+                        # nous permet de gagner en efficacité
         index = 0
         tree = Graph(self.nodes)  # on crée l'arbre que l'on va renvoyer.
         while tree.nb_edges != self.nb_nodes - 1:
@@ -152,20 +159,18 @@ class Graph:
             if find(src) != find(dest):
                 tree.add_edge(src, dest, power)
                 union(src, dest)
-        return tree
+        return tree #on rajoute les arêtes si elles n'ont pas le même parent. Dans ce cas on les relie dans la structure union.
 
 
-    def find_path(self, src, dest,pre_process):
-        root=pre_process[0]
+    def find_path(self, src, dest,pre_process): #on réalise cette fonction afin d'optimiser le temps de recherche d'un chemin dans un arbre connexe.
+        root=pre_process[0] #on appelle notre pré-processing 
         src_chemin=[src]
         dest_chemin=[dest]
-        prof = pre_process[3]
-        dads = pre_process[4]
-        dads[root] = root
         profondeur, fathers = pre_process[1],pre_process[2]
         src_ligne=profondeur[src]
         dest_ligne=profondeur[dest]
-        if src_ligne <= dest_ligne:
+        if src_ligne <= dest_ligne: #on récupère la profondeur des deux noeuds dans l'arbre, et on remonte dans l'arbre jusqu'à trouver le premier 
+            # ancêtre commun aux deux noeuds. De plus, on garde en tête le chemin parcouru par chacun des noeuds.
             while src_ligne < dest_ligne:
                 dest=fathers[dest]
                 dest_ligne=profondeur[dest]
@@ -178,7 +183,7 @@ class Graph:
                 src_ligne = profondeur[src]
                 src_chemin.append(src)
             src_chemin.pop()
-            return src_chemin + dest_chemin[::-1]
+            return src_chemin + dest_chemin[::-1] # on concatène les deux chemins, afin d'avoir, de manière optimale, le (seul) chemin reliant nos deux noeuds.
         elif src_ligne >= dest_ligne:
             while src_ligne > dest_ligne:
                 src = fathers[src]
@@ -195,7 +200,8 @@ class Graph:
             return src_chemin + dest_chemin[::-1]
 
     def min_power_chemin(self, chemin):  # minimum de puissance nécessaire pour pouvoir faire un trajet donnée
-        puissance = 0
+        puissance = 0 #comme nous travaillons sur un arbre, le chemin est unique, donc il suffit juste de parcourir chaque arête de notre chemin,
+        # et de garder la plus grande puissance.
         for i in range(len(chemin) - 1):
             depart = self.graph[chemin[i]]
             for voisin in depart :
@@ -205,7 +211,7 @@ class Graph:
                     break  # on sort de la boucle sur les voisins dès qu'on a trouvé le bon voisin
         return puissance
 
-    def min_power_tree(self, src, dest, pre_process):
+    def min_power_tree(self, src, dest, pre_process): # on renvoie notre chemin le plus court; ainsi que sa puissance.
         chemin = self.find_path(src, dest, pre_process)
         return chemin, self.min_power_chemin(chemin)
     # Pour les network.x.in on a qu'une seule composante connexe donc pas besoin d'utiliser les composantes connexes.
@@ -228,6 +234,7 @@ def graph_from_file(filename):
     return g
 
 
+#liste des network, des routes.in et routes.out
 network1 = "/Users/adrien/Desktop/ENSAE/M1/Cours ENSAE S1/Info/projetS2/input/network.1.in"
 network2 = "/Users/adrien/Desktop/ENSAE/M1/Cours ENSAE S1/Info/projetS2/input/network.2.in"
 network3 = "/Users/adrien/Desktop/ENSAE/M1/Cours ENSAE S1/Info/projetS2/input/network.3.in"
@@ -261,24 +268,25 @@ out8 = "/Users/adrien/Desktop/ENSAE/M1/Cours ENSAE S1/Info/projetS2/input/routes
 out9 = "/Users/adrien/Desktop/ENSAE/M1/Cours ENSAE S1/Info/projetS2/input/routes.9.out"
 out10 = "/Users/adrien/Desktop/ENSAE/M1/Cours ENSAE S1/Info/projetS2/input/routes.10.out"
 
+
 def draw_graph(graphe,chemin):
-    dot = graphviz.Graph()
+    dot = graphviz.Graph() #on crée un graphique à l'aide de la bibliothèque graphviz
     for u, v,w in graphe.edges:
         dot.edge(str(u), str(v))
     for u, v in zip(chemin, chemin[1:]):
-        dot.edge(str(u), str(v), color='red')
+        dot.edge(str(u), str(v), color='red') # on ajoute les arêtes que l'on souhaite colorier pour les mettre en évidence.
     return dot.render( format='png')
 
+
 def question_10(route, network,N):  # on cherche à estimer grossièrement le temps de calcul de plusieurs graphes. L'on remarque que ce temps est considérable.
-    s = graph_from_file(network)
-    g= s.kruskal() # à retirer
+    g = graph_from_file(network)
     with open(route, "r") as file:
         n = int(file.readline().split()[0])
         start = time.time()
         for i in range(N):
             edge = list(map(int, file.readline().split()))
             dep, arr, utilite = edge
-            g.min_power_tree(dep, arr,pre_process)  # on calcule la puissance minimale pour chaque trajet de routes.x.in
+            g.min_power(dep, arr,pre_process)  # on calcule la puissance minimale pour chaque trajet de routes.x.in
         end = time.time()
         elapsed = end - start
         total = (elapsed / N) * n
@@ -305,26 +313,27 @@ def question_15(route, network, out):  # on cherche à estimer le temps de calcu
         total = end - start
         fichier.close()
         return f' Temps total : {total:.5}s'  # on renvoie le temps total
+#On remarque que le temps a considérablement diminué, notre fonction crée et rempli notre dossier routes.x.out entre 1sec et 120sec.
 
 
-
-def dfs(graph, start, profondeur,fathers,visited=None,index=0):
+def dfs(graph, start, profondeur,fathers,visited=None,index=0): #nous réalisons un dfs de notre graphe afin de récupérer, pour chaque arête 
+    # sa profondeur dans le graphe, et son père.
     if visited is None:
         visited = set()  # ensemble de sommets visités
     visited.add(start)  # marquer le sommet comme visité
     for neighbor in graph[start]:
             if neighbor[0] not in visited:  # parcourir les voisins non visités
                 index+=1
-                profondeur[neighbor[0]] += index
-                fathers[neighbor[0]] = start
+                profondeur[neighbor[0]] += index #on augmente la profondeur à chaque fois que l'on descend dans le graphe
+                fathers[neighbor[0]] = start #on récupère le père de chaque noeud
                 dfs(graph, neighbor[0], profondeur,fathers,visited,index)  # appel récursif pour visiter chaque voisin non visité
             else:
                 pass
     return profondeur,fathers
 
 
-
-#Pre_process :
+#Nous réalisons une étape de pré-processing afin d'améliorer considérablement la vitesse de nos algorithmes. Nous récupérons dans celui-ci : 
+# la profondeur de chaque noeud, et le père de chaque noeud.
 g = graph_from_file(network9)
 s = g.kruskal()
 root=s.nodes[0]
