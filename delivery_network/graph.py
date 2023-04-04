2bf28981cc41769f19ce72a16f023cddac25b535
oktayagb <126254961+oktayagb@users.noreply.github.com>
2023-04-04 17:22:44 +0200

    Update graph.py

--- delivery_network/graph.py
+++ delivery_network/graph.py
@@ -216,21 +216,7 @@ def graph_from_file(filename):
     return g
 
 
-def find(node):
-    if parents[node] != node:
-        parents[node] = find(parents[node])
-    return parents[node]  # la fonction find est une fonction récursive qui permet de renvoyer le parent d'un noeud
-def union(x, y):
-    x_racine = find(x)
-    y_racine = find(y)
-    if x_racine != y_racine:
-        if rang[x_racine] < rang[y_racine]:
-            parents[x_racine] = y_racine
-        else:
-            parents[y_racine] = x_racine
-            if rang[x_racine] == rang[y_racine]:
-                rang[x_racine] = rang[x_racine] + 1 #la fonction union permet de lier les noeuds entre eux, l'utilisation de 'racine' 
-                # nous permet de gagner en efficacité
+
 
 
 def kruskal(graph):
@@ -238,6 +224,21 @@ def kruskal(graph):
         edges.sort(key=lambda x: x[2])  # on trie les chemins par ordre croissant de puissance
         parents = {node: node for node in graph.nodes}  # on initialise les parents de chaque noeud.
         rang = {node: 0 for node in graph.nodes} #on itinialise le rang de chaque noeud
+        def find(node):
+            if parents[node] != node:
+                parents[node] = find(parents[node])
+            return parents[node]  # la fonction find est une fonction récursive qui permet de renvoyer le parent d'un noeud
+        def union(x, y):
+            x_racine = find(x)
+            y_racine = find(y)
+            if x_racine != y_racine:
+                if rang[x_racine] < rang[y_racine]:
+                    parents[x_racine] = y_racine
+                else:
+                    parents[y_racine] = x_racine
+                    if rang[x_racine] == rang[y_racine]:
+                        rang[x_racine] = rang[x_racine] + 1 #la fonction union permet de lier les noeuds entre eux, l'utilisation de 'racine' 
+                        # nous permet de gagner en efficacité
         index = 0
         tree = Graph(graph.nodes)  # on crée l'arbre que l'on va renvoyer.
         while tree.nb_edges != graph.nb_nodes - 1:
