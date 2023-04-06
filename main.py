d24533be6779e1e5a0e08fcc6f06297bb48b0dc9
adrienmathieu <126259878+adrienmathieu@users.noreply.github.com>
2023-04-06 20:30:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ class Graph:
                     visited.add(neighbor)  # on le marque comme visité
                     queue.append((neighbor, path + [neighbor]))  # on rajoute à la pile ce noeud et on actualise le parcours en rajoutant le noeud
         return None  # on renvoie None seulement si après avoir tout parcouru on ne trouve pas de chemin de puissance minimal, ou que les éléments sont dans 2 compo connexes
-    # La complexité est en O(nb_edges)
+    # La complexité est en O(nb_edges + nb_nodes)
 
     
     
@@ -148,7 +148,7 @@ class Graph:
                         current_path.append(neighbor[0]) #on rajoute le noeud au trajet
                         dfs(neighbor[0]) #procédé récursif sur chaque noeud
                         current_path.pop()
-            visited.remove(node)
+                visited.remove(node)
 
         dfs(src)
         return paths
@@ -170,10 +170,6 @@ class Graph:
 
 
 
-
-
-
-#########(remarque du prof)on met trop de vas particulier pas besoin de pre_process et pas besoin de faire le egal et elif return fin_path(dest,src)
     def find_path(self, src, dest,profondeur,fathers): #on réalise cette fonction afin d'optimiser le temps de recherche d'un chemin dans un arbre connexe.
         src_chemin=[src]
         dest_chemin=[dest]
@@ -255,7 +251,6 @@ def graph_from_file(filename):
             else:
                 raise Exception("Format incorrect")
     return g
-
 
 
 
@@ -466,3 +461,4 @@ def draw_allocations_rouge_bleu(graphe,liste_chemin): #forme de liste_chemin=[(c
         dot.edge(str(u), str(v))
 
     return dot.render( format='png')
+#####################################################################################################################################
